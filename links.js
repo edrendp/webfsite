76632c74dffc433583b3fb94dc2a9ea35c273454
edrendp <148534139+edrendp@users.noreply.github.com>
2026-01-30 09:35:18 +0800

    Modify TikTok link in socialMediaLinks
    Updated TikTok link to the latest version.

--- links.js
+++ links.js
@@ -2,6 +2,5 @@
 const socialMediaLinks = {
     facebook: "https://web.facebook.com/edrend.p",
     instagram: "https://www.instagram.com/edrend.p/",
-    tiktok: "https://www.tiktok.com/@edrendp?_r=1&_t=ZS-93UTo9nEeOb"
-
+    tiktok: "https://www.tiktok.com/@edrendp?_r=1&_t=ZS-93UU5acrcnc"
 };
