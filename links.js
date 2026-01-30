b7cd288cbe30d7cc3908b2feb6bac299ef656004
edrendp <148534139+edrendp@users.noreply.github.com>
2026-01-30 09:30:49 +0800

    Add TikTok link to social media links

--- links.js
+++ links.js
@@ -1,5 +1,7 @@
 // links.js
 const socialMediaLinks = {
     facebook: "https://web.facebook.com/edrend.p",
-    instagram: "https://www.instagram.com/edrend.p/"
+    instagram: "https://www.instagram.com/edrend.p/",
+    tiktok: "https://www.tiktok.com/@edrendp?_r=1&_t=ZS-93UTo9nEeOb"
+
 };
